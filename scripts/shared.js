499475cd3ed01a72db0568c85cdbe1cf856740b8
Michael Colavita <colavitam@gmail.com>
2013-08-12 21:12:12 -0400

    Corrections, basic information.

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,6 +1,6 @@
 function rename() {
 	var name=document.getElementById("tabFrame").contentWindow.document.title;
-	name="Westford Academy Programming Team: "+name;
+	name="Westford Academy Math Team: "+name;
 	document.title=name;
 	console.log(name);
 }
